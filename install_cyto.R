2d0661c91d9307f8462a7df100623ef1509c3f62
Jonas Schulte-Schrepping <s6jsschu@uni-bonn.de>
2023-06-29 16:35:41 +0200

    Try to fix bug with cytometry analysis package installation - part3

--- install_cyto.R
+++ install_cyto.R
@@ -3,7 +3,7 @@ library(pkgbuild)
 
 ## Install cytoverse
 remotes::install_github("RGLab/cytoverse")
-cytoverse::cytoverse_update(upgrades_approved = TRUE)
+cytoverse::cytoverse_update(upgrades_approved = TRUE, branch="3.17")
 
 ## Install cydar
 devtools::install_version("cydar", version="1.24.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
