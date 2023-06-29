bc408056bb4cbe46b05c932db4ff187458b3cdcd
Jonas Schulte-Schrepping <s6jsschu@uni-bonn.de>
2023-06-29 16:32:51 +0200

    Try to fix bug with cytometry analysis package installation - part2

--- install_cyto.R
+++ install_cyto.R
@@ -3,18 +3,12 @@ library(pkgbuild)
 
 ## Install cytoverse
 remotes::install_github("RGLab/cytoverse")
-cytoverse::cytoverse_update()
+cytoverse::cytoverse_update(upgrades_approved = TRUE)
 
-devtools::install_version("flowCore", version="2.12.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
-devtools::install_version("flowStats", version="4.12.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
-devtools::install_version("CytoML", version="2.12.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
-devtools::install_version("flowWorkspace", version="4.12.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
-devtools::install_version("ggcyto", version="1.28.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
-devtools::install_version("openCyto", version="2.12.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
+## Install cydar
 devtools::install_version("cydar", version="1.24.0", repos=c("https://bioconductor.org/packages/3.17/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
 
-
-## CytoExploreR
+## Install CytoExploreR
 # CytoExploreRData
 devtools::install_github("DillonHammill/CytoExploreRData")
 # CytoExploreR
