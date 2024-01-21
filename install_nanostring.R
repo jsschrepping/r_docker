9f0c98db0da819d1cf316171f624f5267e94bec0
Jonas Schulte-Schrepping <s6jsschu@uni-bonn.de>
2024-01-21 14:46:34 +0100

    Fix bugs for build.

--- install_nanostring.R
+++ install_nanostring.R
@@ -3,9 +3,9 @@ library(pkgbuild)
 
 # Bioconductor packages
 # Nanostring packages
-devtools::install_version("NanoStringNCTools", version="1.2.0", repos=c("https://bioconductor.org/packages/3.14/bioc","http://cran.us.r-project.org"),upgrade = "never", force = TRUE)
-devtools::install_version("NanoStringQCPro", version="1.26.0", repos=c("https://bioconductor.org/packages/3.14/bioc","http://cran.us.r-project.org"),upgrade = "never", force = TRUE)
-devtools::install_version("RUVSeq", version="1.28.0", repos=c("https://bioconductor.org/packages/3.14/bioc","http://cran.us.r-project.org"),upgrade = "never", force = TRUE)
+devtools::install_version("NanoStringNCTools", version="1.2.0", repos=c("https://bioconductor.org/packages/3.14/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
+devtools::install_version("NanoStringQCPro", version="1.26.0", repos=c("https://bioconductor.org/packages/3.14/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
+devtools::install_version("RUVSeq", version="1.28.0", repos=c("https://bioconductor.org/packages/3.14/bioc","https://cran.uni-muenster.de/"),upgrade = "never", force = TRUE)
 
 # Github Packages
 devtools::install_github('bhattacharya-a-bt/NanoNormIter')
